d962c6cbf9e71074386b3c75817c1e710623d49c
Taras Dolynyuk <trsdln@gmail.com>
2017-04-03 11:38:11 +0300

    Include lib folder at lines-of-code

--- lib/lines-of-code.sh
+++ lib/lines-of-code.sh
@@ -4,7 +4,7 @@ function lines-of-code {
    cat \
    <(git ls-files) \
    <(git submodule foreach 'git ls-files | sed "s|^|$path/|"') \
-   | grep -E '^(imports|client|server|packages).+\.js' \
+   | grep -E '^(imports|client|server|packages|lib).+\.js' \
    | xargs wc -l \
    | tail -n 1
 }
